9bc9d38a72f754e8af8bc598e8bbc50d2ee174bc
OlgaZakharovaAB <91073511+OlgaZakharovaAB@users.noreply.github.com>
2021-09-22 20:49:48 +0300

    Add files via upload

---
+++ 1.square.lab4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    cout << "Площадь: " << a*b << endl;
+    cout << "Периметр: " << 2*(a + b);
+
+    return 0;
+}
