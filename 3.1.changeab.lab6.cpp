4604a81f44386eead0e624239cca3297ed27315c
OlgaZakharovaAB <91073511+OlgaZakharovaAB@users.noreply.github.com>
2021-10-12 12:25:27 +0300

    Add files via upload

---
+++ 3.1.changeab.lab6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b;
+    c = a;
+    a = b;
+    b = c;
+
+    cout << a << endl << b;
+    return 0;
+}
