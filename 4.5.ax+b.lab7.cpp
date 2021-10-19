a5ad0c15ed9687d3681f7ba659930a9fa9cb27d2
OlgaZakharovaAB <91073511+OlgaZakharovaAB@users.noreply.github.com>
2021-10-19 12:56:54 +0300

    Add files via upload

---
+++ 4.5.ax+b.lab7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, x;
+    cout << "Input a!=0 and b: ";
+    cin >> a >> b;
+    x = -b / a;
+    cout << "x = " << x;
+
+    return 0;
+}
