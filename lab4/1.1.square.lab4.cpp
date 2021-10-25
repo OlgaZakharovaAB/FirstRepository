35d54d644d0629115f6ce15cfbccde4ce0794c49
Olga Zakharova <black.amaranth@mail.ru>
2021-10-25 19:15:40 +0300

    Create 1.1.square.lab4.cpp

---
+++ lab4/1.1.square.lab4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    cout << "Площадь: " << a*b << endl;
+    cout << "Периметр: " << 2*(a + b);
+
+    return 0;
+}
+
+// Я использую этот комментарий для проверки работы GitHub
