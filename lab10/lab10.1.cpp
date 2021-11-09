64bb4a7d3a218b81e8479daf0c3f5837a8515cc3
Olga Zakharova <black.amaranth@mail.ru>
2021-11-09 12:55:23 +0300

    Create lab10.1.cpp

---
+++ lab10/lab10.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	cin >> a >> b;
+	if ((a > 2) and (b <= 3)) {
+		cout << "Высказывание 'Справедливо неравенство  " << a << " > 2 и " << b << " <= 3' истино";
+	}
+	else {
+		cout << "Высказывание 'Справедливо неравенство  " << a << " > 2 и " << b << " <= 3' ложно";
+	}
+	return 0;
+}
