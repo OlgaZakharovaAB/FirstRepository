4f1052e6b09f16abe2cc36c164b9ad74e6b3964f
Olga Zakharova <black.amaranth@mail.ru>
2021-12-02 14:27:11 +0300

    Create lab15.3.cpp

---
+++ lab15/lab15.3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+float RingS(float a, float b) {
+	float s1 = 3.14 * a*a;
+	float s2 = 3.14 * b*b;
+	return s1 - s2;
+}
+
+int main() {
+	float r1, r2;
+	int i;
+	for (i = 0; i < 3; i++) {
+		cin >> r1 >> r2;
+		cout << RingS(r1, r2) << endl;
+	}
+}
